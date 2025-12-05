b3bf09f25af5f059400f4d58fad2c4c7ebac7488
Taranveer Singh <taran.impact@gmail.com>
2025-12-05 16:53:39 -0500

    fix: Restore webpack config to ignore pino-pretty and other backend deps

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
